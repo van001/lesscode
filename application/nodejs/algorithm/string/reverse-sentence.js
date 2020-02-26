758cc51cd979d0d3e800e51e82714c2dcc0ec18d
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-02-26 09:19:58 -0800

    added reverse-sentence

---
+++ application/nodejs/algorithm/string/reverse-sentence.js
@@ -0,0 +1,24 @@
+/** 
+Problem : reverse words in a sentence :: 'my name is neelesh' -> 'neelesh is name my'
+1. reverse the string
+2. split string into list of strings
+3. Apply reverse operation to all items in the list
+4. Convert List back to String
+
+**/
+
+/** Toolset **/ 
+const { $, $p, l2String2, lapply2, space, s2List2, sreverse} = require('../lc-core')
+
+/** Function */
+const reverseSentence = $p(l2String2(space), lapply2(sreverse), s2List2(space), sreverse)
+
+/** Test */
+const data = [
+    ['my name is neelesh','neelesh is name my']
+]
+
+data.forEach(val => console.assert(reverseSentence(val[0])==val[1], ` ${val}`))
+
+
+
