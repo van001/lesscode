565a7e1ae5583b0f2eca80b0b6a8d198b911cbb6
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-02-26 21:39:37 -0800

    fin non repear char

---
+++ application/nodejs/algorithm/string/first-non-repeat-char.js
@@ -0,0 +1,21 @@
+/**
+ * Print first non repeated character from String
+ * e.g :  "morning" should print "m".
+ * 
+ * 1. Transform String to List
+ * 2. Tramsform List to Map (histogram).
+ * 3. Filter all the keys from the Map that is > 1
+ * 4. Pick the 1st Key
+ * 
+ */
+
+ /** Toolset */
+const { histogram, l2Map2, eq2, mfilter2, assert, $p, blank, s2List2 } = require('../../lc-core')
+
+/** Function */
+const mXheadKey = map => Object.keys(map)[0]
+const sfirstNonRepeatChar = $p(mXheadKey,mfilter2(eq2(1)), l2Map2(histogram), s2List2(blank))
+
+const data =[['morning','m']]
+
+data.forEach( val => assert(sfirstNonRepeatChar(val[0]))(val[1])(`${val}`))
