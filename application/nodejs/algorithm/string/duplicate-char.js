8d1dc3b43c3e6b6c9f53364c11e8289f322c619e
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-02-26 16:37:53 -0800

    duplicate-char.js

--- application/nodejs/algorithm/string/duplicate-char.js
+++ application/nodejs/algorithm/string/duplicate-char.js
@@ -4,14 +4,21 @@
  */
 
 /** Toolset */
-const { histogram, l2Map2, gt2, mfilter2, assert, $, blank, s2List2 } = require('../../lc-core')
+const { histogram, l2Map2, gt2, mfilter2, assert, $p, blank, s2List2 } = require('../../lc-core')
 
 /** Function */
-const sdulpicateChar = $(mfilter2(gt2(1)), l2Map2(histogram), s2List2(blank))
+const sdulpicateChar = $p(mfilter2(gt2(1)), l2Map2(histogram), s2List2(blank))
 
 /** Test */
 
-const data = [['Programming',{ r: 2, g: 2, m: 2 }]]
+const data = [['Programming', { r: 2, g: 2, m: 2 }]]
 
-data.forEach (val => assert(sdulpicateChar(val[0]))(val[1])(`${val}`))
+data.forEach(val => assert(sdulpicateChar(val[0]))(val[1])(`${val}`))
+
+/**
+ * Output :
+   [ 'P', 'r', 'o', 'g', 'r', 'a', 'm', 'm', 'i', 'n', 'g' ]
+   { P: 1, r: 2, o: 1, g: 2, a: 1, m: 2, i: 1, n: 1 }
+   { r: 2, g: 2, m: 2 }
+ */
 
