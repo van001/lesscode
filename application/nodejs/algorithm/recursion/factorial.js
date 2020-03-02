7a83eeec31a6a8071af499acbb4c4656d4cc234c
Neelesh Vaikhary <neeleshvaikhary@Neeleshs-iMac.local>
2020-03-02 13:44:22 -0800

    factorial via composition

--- application/nodejs/algorithm/recursion/factorial.js
+++ application/nodejs/algorithm/recursion/factorial.js
@@ -3,18 +3,18 @@
  * The factorial of a number 'n' is the product of all positive integers less than or equal to 'n'. 
  * So, the factorial of 6 would be 5*4*3*2*1 = 120. The factorial of 0 is 1. 
  * 
- * 1. Write a recursive function.
+ * 1. Since there is no category transformation write a recursive function.
  * 2. Optimize it using accumulator for the language that support tail recursion.
  */
 
  /** Toolset */
-const {eq2, assert} = require('../../lc-core')
-
+const {eq2, assert,$} = require('../../lc-core')
 
 /** Function */
+// factorial:: Integer -> Integer
 const factorial = n => {
     const factorial2 = acc => n => eq2(0)(n)? acc : factorial2(acc * n)(n-1)
-    return factorial2(1)(n)
+    return $(factorial2(1))(n)
 }
 
 /** Test */
